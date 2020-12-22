094ba7af9bb7d015abd3a28a48ac76d131921e32
hp <tiwari.mah.8898@gmail.com>
2020-12-22 22:20:27 +0530

    Making Responsive

--- app/http/controllers/admin/orderController.js
+++ app/http/controllers/admin/orderController.js
@@ -1,5 +1,5 @@
 
-const order = require('../../../models/order')
+
 
 const Order = require('../../../models/order')
 
@@ -7,7 +7,7 @@ const Order = require('../../../models/order')
 function orderController() {
     return {
         index(req, res) {
-            order.find({ status: { $ne: 'completed'} }, null, { sort: { 'createdAt': -1 }}).populate('customerId', '-password').exec((err, orders) => {
+            Order.find({ status: { $ne: 'completed'} }, null, { sort: { 'createdAt': -1 }}).populate('customerId', '-password').exec((err, orders) => {
                 if(req.xhr) {
                     return res.json(orders)
                 } else {
